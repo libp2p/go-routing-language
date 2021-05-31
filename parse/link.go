1fa52fc17e65f1b3b842bb02433c521e8ad3d090
Petar Maymounkov <petarm@gmail.com>
2021-05-31 09:46:48 -0700

    add ParseLink

---
+++ parse/link.go
@@ -0,0 +1,28 @@
+package parse
+
+import (
+	"fmt"
+
+	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-routing-language/syntax"
+)
+
+// ParseLink parses formulas of the form link(CID:STRING) to a libp2p cid.
+func ParseLink(ctx *ParseCtx, src syntax.Node) (cid.Cid, error) {
+	p, ok := src.(syntax.Predicate)
+	if !ok {
+		return cid.Cid{}, fmt.Errorf("not a predicate")
+	}
+	if p.Tag != "link" {
+		return cid.Cid{}, fmt.Errorf("tag is not link")
+	}
+	if len(p.Positional) != 1 {
+		return cid.Cid{}, fmt.Errorf("expecting one argument")
+	}
+	s, ok := p.Positional[0].(syntax.String)
+	if !ok {
+		return cid.Cid{}, fmt.Errorf("expecting a string argument")
+	}
+	c, err := cid.Decode(s)
+	return c, err
+}
---
+++ parse/parse.go
@@ -0,0 +1,24 @@
+package parse
+
+import (
+	"github.com/libp2p/go-routing-language/syntax"
+)
+
+type Parser interface {
+	Parse(ctx *ParseCtx, src syntax.Node) (interface{}, error)
+}
+
+type ParseCtx struct {
+	keys map[interface{}]interface{}
+}
+
+func (x *ParseCtx) Set(key interface{}, value interface{}) interface{} {
+	v := x.keys[key]
+	x.keys[key] = value
+	return v
+}
+
+func (x *ParseCtx) Get(key interface{}) (interface{}, bool) {
+	v, ok := x.keys[key]
+	return v, ok
+}
